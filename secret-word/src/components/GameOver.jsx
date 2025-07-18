46d6268f91673de537e3e4d414d11153786604fd
Jefferson Formigoni Araujo <jeffersonformigoni86@gmail.com>
2025-07-18 14:10:50 -0300

    feat: Adicionar novo componentes para o jogo

--- secret-word/src/components/GameOver.jsx
+++ secret-word/src/components/GameOver.jsx
@@ -1,9 +1,15 @@
 import "./GameOver.css";
 
-const GameOver = () => {
+const GameOver = ({ retry }) => {
   return (
-    <div>GameOver</div>
-  )
-}
+    <div>
+      <h1>GameOver</h1>
+      <p>Obrigado por jogar!</p>
+      <p>Clique no botão abaixo para reiniciar o jogo</p>
+      <button onClick={retry}>Reiniciar o jogo</button>
+      <p>Boa sorte na próxima vez!</p>
+    </div>
+  );
+};
 
-export default GameOver
+export default GameOver;
